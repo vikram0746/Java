00c9873d6c0eb10c801317dbe8b7882544331382
vikram0746 <ramanvikram75@gmail.com>
2025-09-24 14:46:34 +0530

    u

--- arr/Median.java
+++ arr/Median.java
@@ -1,8 +1,12 @@
 package arr;
-
+import java.util.*;
 public class Median {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		int []arr= {1,2,5,4,7,9,6};
+		Arrays.sort(arr);
+		int b=arr.length/2;
+		System.out.println("median:"+arr[b]);
+		System.out.print(Arrays.toString(arr));
+		
 	}
-
 }
